ddfca42ba990d507a239805e79ef3be5a760a5dd
Alex2920 <alexleguyadec@gmail.com>
2017-11-07 14:33:15 +0100

    Version N°1

---
+++ application/views/v_connexion.php
@@ -0,0 +1,26 @@
+﻿<?php
+	$this->load->helper('url');
+	$path = base_url();
+?>
+
+<div id="contenu">
+	<h2>Identification utilisateur</h2>
+
+	<?php if (isset($erreur))	echo '<div class ="erreur"><ul><li>'.$erreur.'</li></ul></div>'; ?>
+
+	<form method="POST" action="<?php echo $path.'c_default/connecter';?>">
+		<p>
+			<label for="login">Login*</label>
+			<input id="login" type="text" name="login"  size="30" maxlength="45"/>
+		</p>
+		<p>
+			<label for="nom">Mot de passe*</label>
+			<input id="mdp"  type="password"  name="mdp" size="30" maxlength="45"/>
+		</p>
+		<p>
+			<input type="submit" value="Valider" name="valider"/>
+			<input type="reset" value="Annuler" name="annuler"/> 
+		</p>
+	</form>
+
+</div>
---
+++ application/views/v_visVoirListeFrais.php
@@ -0,0 +1,65 @@
+<?php
+	$this->load->helper('url');
+?>
+
+<div id="contenu">
+	<h2>Ma fiche de frais du mois <?php echo $numMois."-".$numAnnee; ?></h2>
+					
+	<div class="corpsForm">
+	  
+		<fieldset>
+			<legend>Eléments forfaitisés</legend>
+			<?php
+				foreach ($lesFraisForfait as $unFrais)
+				{
+					$idFrais = $unFrais['idfrais'];
+					$libelle = $unFrais['libelle'];
+					$quantite = $unFrais['quantite'];
+
+					echo 
+					'<p>
+						<label for="'.$idFrais.'">'.$libelle.'</label>
+						<input type="text" id="'.$idFrais.'" name="lesFrais['.$idFrais.']" size="10" maxlength="5" value="'.$quantite.'" />
+					</p>
+					';
+				}
+			?>
+		</fieldset>
+		<p></p>
+	</div>
+
+	
+	<table class="listeLegere">
+		<caption>Descriptif des éléments hors forfait</caption>
+		<tr>
+			<th >Date</th>
+			<th >Libellé</th>  
+			<th >Montant</th>  
+			<th >&nbsp;</th>              
+		</tr>
+          
+		<?php    
+			foreach( $lesFraisHorsForfait as $unFraisHorsForfait) 
+			{
+				$libelle = $unFraisHorsForfait['libelle'];
+				$date = $unFraisHorsForfait['date'];
+				$montant=$unFraisHorsForfait['montant'];
+				$id = $unFraisHorsForfait['id'];
+				echo 
+				'<tr>
+					<td class="date">'.$date.'</td>
+					<td class="libelle">'.$libelle.'</td>
+					<td class="montant">'.$montant.'</td>
+					<td class="action">'.
+					anchor(	"c_visiteur/supprFrais/$id", 
+							"Supprimer ce frais", 
+							'title="Suppression d\'une ligne de frais" onclick="return confirm(\'Voulez-vous vraiment supprimer ce frais ?\');"'
+						).
+					'</td>
+				</tr>';
+			}
+		?>	  
+                                          
+    </table>
+
+</div>
